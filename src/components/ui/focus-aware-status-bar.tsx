a48087d3eb898d77558c57b0179aca866d0e0a1e
Rishab Khivsara <rishabjain3500@gmail.com>
2025-01-22 10:29:29 +0530

    Refactor: Ability to manually modify state

--- src/components/ui/focus-aware-status-bar.tsx
+++ src/components/ui/focus-aware-status-bar.tsx
@@ -4,12 +4,15 @@ import * as React from 'react';
 import { Platform } from 'react-native';
 import { SystemBars } from 'react-native-edge-to-edge';
 
-type Props = { hidden?: boolean };
-export const FocusAwareStatusBar = ({ hidden = false }: Props) => {
+type Props = { hidden?: boolean; style?: 'light' | 'dark' };
+export const FocusAwareStatusBar = ({ hidden = false, style }: Props) => {
   const isFocused = useIsFocused();
   const { colorScheme } = useColorScheme();
 
-  if (Platform.OS === 'web') return null;
+  const finalStyle = style || colorScheme;
 
-  return isFocused ? <SystemBars style={colorScheme} hidden={hidden} /> : null;
+  if (Platform.OS === 'web') return null;
+  console.log(`Active theme: ${colorScheme}`);
+
+  return isFocused ? <SystemBars style={finalStyle} hidden={hidden} /> : null;
 };
